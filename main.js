348f243bb639e5c4771b8f813177a483613146f8
Ubuntu <ubuntu@ip-172-31-31-53.ec2.internal>
2022-06-22 15:23:30 +0000

    resetbutton_syuusei2

--- main.js
+++ main.js
@@ -1,68 +1,65 @@
-/*global $*/
-(function(){
-  'use strict';
-
-  var timer = document.getElementById('timer');
-  var start = document.getElementById('start');
-  var stop = document.getElementById('stop');
-  var reset = document.getElementById('reset');
+  const timer = document.querySelector(".timer");
+  const start = document.querySelector(".start");
+  const stop = document.querySelector(".stop");
+  const reset = document.querySelector(".reset");
 
   var startTime;
   var elapsedTime = 0;
   var timerId;
   var timeToadd = 0;
 
-function updateTimetText(){
+function updateTimeText(){
   var h = Math.floor(elapsedTime / 60000 / 60);
   var m = Math.floor(elapsedTime / 60000);
   var s = Math.floor(elapsedTime % 60000 / 1000);
   var ms = elapsedTime % 1000;
 
-      h = ('' + h).slice(-2);
-      m = ('' + m).slice(-2); 
-      s = ('' + s).slice(-2);
-      ms = ('' + ms).slice(-1);
+      h = ("" + h).slice(-2);
+      m = ("" + m).slice(-2); 
+      s = ("" + s).slice(-2);
+      ms = ("" + ms).slice(-1);
       
-      timer.textContent = h + ':' + m + ':' + s + ':' + ms;
+      timer.textContent = h + ":" + m + ":" + s + ":" + ms;
       
-    }
+}
 
 function countUp(){
-      timerId = setTimeout(function(){
-      elapsedTime = Date.now() - startTime + timeToadd;
-      updateTimetText()
-      countUp();
-      
-      },10);
-    }
+  timerId = setTimeout(function(){
+  elapsedTime = Date.now() - startTime + timeToadd;
+  updateTimeText()
+  countUp();
+  
+  },10);
+}
 
-      start.addEventListener('click',function(){
-      startTime = Date.now();
-      countUp();
-      
-  　　  start.setAttribute("disabled", true);
-    　stop.removeAttribute("disabled");
-  　　　reset.removeAttribute("disabled");
-  　  　
-    });
+function startTimer() {
+  startTime = Date.now();
+  countUp();
+  
+  start.setAttribute("disabled", true);
+  stop.removeAttribute("disabled");
+  reset.removeAttribute("disabled");
+  
+}
 
-    stop.addEventListener('click',function(){
-    clearTimeout(timerId);
-    timeToadd += Date.now() - startTime;
-    
-    stop.setAttribute("disabled", true);
-    start.removeAttribute("disabled");
-    
-    });
+function stopTimer() {
+  clearInterval(timerId);
+  timeToadd += Date.now() - startTime;
+  
+  stop.setAttribute("disabled", true);
+  start.removeAttribute("disabled");
+  
+}
 
-    reset.addEventListener('click',function(){
-    elapsedTime = 0;
-    timeToadd = 0;
-    updateTimetText();
-    
-    start.removeAttribute("disabled");
-    stop.setAttribute("disabled", true);
-    reset.setAttribute("disabled", true);
-    
-    });
-})();
+function resetTimer() {
+  clearInterval(timerId);
+  timeToadd = 0;
+  elapsedTime = 0;
+  updateTimeText();
+  
+  start.removeAttribute("disabled");
+  stop.setAttribute("disabled", true);
+  reset.setAttribute("disabled", true);
+  
+}
+
